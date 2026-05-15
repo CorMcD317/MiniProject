68b4b3702bd388f6c64647eff8da5cbb9b834ff6
mcdoucor000 <mcdoucor000@warrern.k12.in.us>
2026-05-15 09:38:50 -0400

    Fixed Nav Bar a bit. added hover to top 5.made list of what I couldn't complete

---
+++ JavaScripts/page4.js
@@ -0,0 +1,40 @@
+const SUPABASE_URL = "https://cvjdmccwopblebapnbcq.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2amRtY2N3b3BibGViYXBuYmNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzE3ODUsImV4cCI6MjA5MTgwNzc4NX0.muiYFkCNxojvIwlhx-LhDubRUK7OpLpp0e5zwRznInE";
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+async function loadBosses() {
+
+    const { data, error } =
+        await supabase
+            .from('boss_stats')
+            .select('boss_name, health_points, damage')
+            .eq('id', 4)
+
+    if (error) {
+        console.error(error)
+        return
+    }
+
+    const tbody =
+        document.querySelector('#SilkTable tbody')
+
+    tbody.innerHTML = ''
+
+    data.forEach(boss => {
+
+        const row =
+            document.createElement('tr')
+
+        row.innerHTML = `
+            <td>${boss.health_points}</td>
+            <td>${boss.damage}</td>
+            <td>${boss.mobility}</td>
+        `
+
+        tbody.appendChild(row)
+
+    })
+}
+
+loadBosses()
